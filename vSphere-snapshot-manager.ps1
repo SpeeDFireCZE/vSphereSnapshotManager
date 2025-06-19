d5df4903ef150d0b956356a0dfb174c26afee2e9
SpeeDFireCZE <142578797+SpeeDFireCZE@users.noreply.github.com>
2025-06-19 15:25:36 +0200

    Update vSphere-snapshot-manager.ps1

--- vSphere-snapshot-manager.ps1
+++ vSphere-snapshot-manager.ps1
@@ -7,7 +7,7 @@ $Icon = Join-Path $env:TEMP "tempicon.ico"
 [IO.File]::WriteAllBytes($Icon, [Convert]::FromBase64String($iconBase64))
 
 #--------------------------------------Sdílené proměnné-----------------------------------------------#
-    $server= "*******"
+    $server= "*****"
 #---------------------------------kontrola dostupnosti--------------------------------------#
 $vcaccess = ping $server -n 1
 if($vcaccess -match "Received = 1"){
@@ -123,9 +123,9 @@ if($Buttonlogin.Text -ne "VM login"){
         $snaptime= Get-Snapshot -VM $line | select-object -expand created
         foreach ($snap in $snaptime){
         $finishtime= $snap.AddMinutes(5).ToString("dd/MM/yyyy HH:mm:ss")
-        $finishtimedatetime= [datetime]::ParseExact("$finishtime", "dd/MM/yyyy h:mm:ss", $null)
+        $finishtimedatetime= [datetime]::ParseExact("$finishtime", "dd/MM/yyyy HH:mm:ss", $null)
         $starttime= $snap.AddMinutes(-59).ToString("dd/MM/yyyy HH:mm:ss")
-        $starttimedatetime= [datetime]::ParseExact("$starttime", "dd/MM/yyyy h:mm:ss", $null)
+        $starttimedatetime= [datetime]::ParseExact("$starttime", "dd/MM/yyyy HH:mm:ss", $null)
         $exit= Get-VIEvent -Entity $line -Types Info -start $starttimedatetime -finish $finishtimedatetime| Where-Object {$_.FullFormattedMessage -imatch 'Task: Create virtual machine snapshot'} | Select-Object -expand UserName | Out-String
         $vysledek = ($exit -split "`n")
     }
